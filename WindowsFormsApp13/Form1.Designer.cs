551e090aa919dc95991dcf9f434af53d8c9486a7
Meryem sıla Fidan <silafidan40@gmail.com>
2025-07-15 16:19:32 +0300

    PROSES
    PROSES

---
+++ WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(48, 54);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(905, 54);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(48, 114);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(905, 49);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(48, 169);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(905, 50);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(48, 288);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 290);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(379, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 61);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 57);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Senkron Interlock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 429);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 60);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Senkron Lock";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(379, 509);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 57);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Senkron Monitor";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(644, 287);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(251, 62);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Sıfırla";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(644, 355);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(251, 57);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Senkron Mutex";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(644, 429);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(251, 60);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Senkron Semaphore";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 665);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        private object monitorObject = new object();
+        private Mutex mutexObject = new Mutex();
+        private Semaphore semaphoreObject = new Semaphore(0, 2);
+        private Thread th1;
+        private Thread th2;
+        private Thread th3;
+
+        long counter = 1;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(DegerArtir1));
+            th2 = new Thread(new ThreadStart(DegerArtir2));
+            th3 = new Thread(new ThreadStart(DegerArtir3));
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+
+        }
+
+        public void DegerArtir1()
+        {
+            for (int i = 1; i <= 100; ++i)
+            {
+                progressBar1.Value += 1;
+                listBox1.Items.Add("Thread 1 calisiyor");
+                Thread.Sleep(10);
+            }
+        }
+
+
+        public void DegerArtir2()
+        {
+            for (int j = 1; j <= 100; ++j)
+            {
+                progressBar2.Value += 1;
+                listBox1.Items.Add("Thread 2 calisiyor");
+                Thread.Sleep(100);
+
+            }
+        }
+        public void DegerArtir3()
+        {
+            for (int k = 1; k <= 100; ++k)
+            {
+                progressBar3.Value += 1;
+                listBox1.Items.Add("Thread 3 calisiyor");
+                Thread.Sleep(150);
+
+            }
+        }
+        public void DegerArtirInterlock1()
+        {
+            try
+            {
+                while (counter <= 300)
+                {
+                    Interlocked.Increment(ref counter);
+                    progressBar1.Value += 1;
+                    listBox1.Items.Add("Thread 1 Calisti");
+                    Thread.Sleep(80);
+                }
+            }
+            catch { }
+        }
+        public void DegerArtirInterlock2()
+        {
+            try
+            {
+                while (counter <= 300)
+                {
+                    Interlocked.Increment(ref counter);
+                    progressBar2.Value += 1;
+                    listBox1.Items.Add("Thread 2 Calisti");
+                    Thread.Sleep(80);
+                }
+            }
+            catch { }
+        }
+        public void DegerArtirInterlock3()
+        {
+            try
+            {
+                while (counter <= 300)
+                {
+                    Interlocked.Increment(ref counter);
+                    progressBar3.Value += 1;
+                    listBox1.Items.Add("Thread 3 Calisti");
+                    Thread.Sleep(80);
+                }
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(DegerArtirInterlock1));
+            th2 = new Thread(new ThreadStart(DegerArtirInterlock2));
+            th3 = new Thread(new ThreadStart(DegerArtirInterlock3));
+
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+
+        }
+        //lock.............
+        public void DegerArtirLock1()
+        {
+            for (int i = 1; i <= 100; ++i)
+            {
+                lock (this)
+                {
+                    progressBar1.Value += 1;
+                    listBox1.Items.Add("Thread 1 Calisti");
+                    Thread.Sleep(10);
+                }
+            }
+        }
+        public void DegerArtirLock2()
+        {
+            for (int j = 1; j <= 100; ++j)
+            {
+                lock (this)
+                {
+                    progressBar2.Value += 1;
+                    listBox1.Items.Add("Thread 2 Calisti");
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        public void DegerArtirLock3()
+        {
+            for (int k = 1; k <= 100; ++k)
+            {
+                lock (this)
+                {
+                    progressBar3.Value += 1;
+                    listBox1.Items.Add("Thread 3 Calisti");
+                    Thread.Sleep(150);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            th1 = new Thread(new ThreadStart(DegerArtirInterlock1));
+            th2 = new Thread(new ThreadStart(DegerArtirInterlock2));
+            th3 = new Thread(new ThreadStart(DegerArtirInterlock3));
+
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+        }
+        //monitor
+        public void DegerArtirMonitor1()
+        {
+            for (int i = 1; i <= 100; ++i)
+            {
+                try
+                {
+                    Monitor.Enter(monitorObject);
+                    progressBar1.Value += 1;
+                    listBox1.Items.Add("Thread 1 Calisti");
+                    Thread.Sleep(10);
+                    Monitor.Pulse(monitorObject);
+                }
+                finally
+                {
+                    Monitor.Exit(monitorObject);
+                }
+            } }
+        public void DegerArtirMonitor2()
+        {
+            for (int j = 1; j <= 100; ++j)
+            {
+                try
+                {
+                    Monitor.Enter(monitorObject);
+                    progressBar2.Value += 1;
+                    listBox1.Items.Add("Thread 2 Calisti");
+                    Thread.Sleep(100);
+                    Monitor.Pulse(monitorObject);
+                }
+                finally
+                {
+                    Monitor.Exit(monitorObject);
+                }
+            } }
+        public void DegerArtirMonitor3()
+        {
+            for (int k = 1; k <= 100; ++k)
+            {
+                try
+                {
+                    Monitor.Enter(monitorObject);
+                    progressBar3.Value += 1;
+                    listBox1.Items.Add("Thread 3 Calisti");
+                    Thread.Sleep(150);
+                    Monitor.Pulse(monitorObject);
+                }
+                finally
+                {
+                    Monitor.Exit(monitorObject);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            th1 = new Thread(new ThreadStart(DegerArtirMonitor1));
+            th2 = new Thread(new ThreadStart(DegerArtirMonitor2));
+            th3 = new Thread(new ThreadStart(DegerArtirMonitor3));
+
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+        }
+        public void DegerArtirMutex1()
+        {
+            for(int i= 1; i<=100; ++i)
+            {
+                mutexObject.WaitOne();
+                progressBar1.Value += 1;
+                listBox1.Items.Add("Thread 1 Calisti");
+                Thread.Sleep(10);
+                mutexObject.ReleaseMutex();
+            }
+        }
+        public void DegerArtirMutex2()
+        {
+            for (int j = 1; j <= 100; ++j)
+            {
+                mutexObject.WaitOne();
+                progressBar2.Value += 1;
+                listBox1.Items.Add("Thread 2 Calisti");
+                Thread.Sleep(100);
+                mutexObject.ReleaseMutex();
+            }
+        }
+        public void DegerArtirMutex3()
+        {
+            for (int k= 1; k <= 100; ++k)
+            {
+                mutexObject.WaitOne();
+                progressBar3.Value += 1;
+                listBox1.Items.Add("Thread 3 Calisti");
+                Thread.Sleep(150);
+                mutexObject.ReleaseMutex();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(DegerArtirMutex1));
+            th2 = new Thread(new ThreadStart(DegerArtirMutex2));
+            th3 = new Thread(new ThreadStart(DegerArtirMutex3));
+
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+        }
+        //semaphore
+        public void DegerArtirSemaphore1()
+        {
+            for(int i=1; i<=100; ++i)
+            {
+                semaphoreObject.WaitOne();
+                progressBar1.Value += 1;
+                listBox1.Items.Add("Thread 1 Calisti");
+                Thread.Sleep(10);
+                semaphoreObject.Release();
+            }
+        }
+        public void DegerArtirSemaphore2()
+        {
+            for (int j = 1; j <= 100; ++j)
+            {
+                semaphoreObject.WaitOne();
+                progressBar2.Value += 1;
+                listBox1.Items.Add("Thread 2 Calisti");
+                Thread.Sleep(100);
+                semaphoreObject.Release();
+            }
+        }
+        public void DegerArtirSemaphore3()
+        {
+            for (int k = 1; k<= 100; ++k)
+            {
+                semaphoreObject.WaitOne();
+                progressBar3.Value += 1;
+                listBox1.Items.Add("Thread 3 Calisti");
+                Thread.Sleep(150);
+                semaphoreObject.Release();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(new ThreadStart(DegerArtirSemaphore1));
+            th2 = new Thread(new ThreadStart(DegerArtirSemaphore2));
+            th3 = new Thread(new ThreadStart(DegerArtirSemaphore3));
+
+
+            th1.Start();
+            th2.Start();
+            th3.Start();
+
+            semaphoreObject.Release();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            th1.Interrupt();
+            th2.Interrupt();
+            th2.Interrupt();
+
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 0;
+
+            progressBar1.Refresh();
+            progressBar2.Refresh();
+            progressBar3.Refresh();
+
+            listBox1.Items.Clear();
+
+            counter = 1;
+
+
+
+        }
+    }
+}
